50f6b175efae3bf6386d177ba42b678a28ed09cb
aliize <ap9387@student.uni-lj.si>
2018-09-12 00:49:39 +0200

    added login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import {GrafiComponent} from "./grafi/grafi.component";
 import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+
 
 const MAINMENU_ROUTES: Routes = [
   //full : makes sure the path is absolute path
   { path: 'grafi', component: GrafiComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
 
 ];
 export const CONST_ROUTING = RouterModule.forRoot(MAINMENU_ROUTES);
